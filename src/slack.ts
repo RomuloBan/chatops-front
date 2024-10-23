01a314ef06a9b675b82bb1be469b6bb9ad4b82c6
romulo.banares <romulo.banares.contractor@bbva.com>
2024-10-24 00:47:11 +0200

    add handle slash command

--- src/slack.ts
+++ src/slack.ts
@@ -1,6 +1,34 @@
 import type { Context } from "@netlify/functions";
 
+import { parse } from "querystring";
+import { slackApi } from "./util/slack";
+
+async function handleSlashCommand(payload: SlackSlashCommandPayload) {
+	switch (payload.command) {
+		case '/foodfight':
+			const response = await slackApi('chat.postMessage', {
+				channel: payload.channel_id,
+				text: 'Things are happening!'
+			})
+
+			if (!response.ok) {
+				console.log(response);
+			}
+			break;
+		default:
+			return new Response(`Command ${payload.command} is not recognized`, { status: 200})
+	}
+
+	return new Response('', { status: 200})
+}
 
 export default async (req: Request, context: Context) => {
-	return new Response('{ "message": "Helloooo"}', { status: 200, headers: { 'Content-Type': 'application/json' } })
+
+	const body = await req.text();
+	const parsedBody = parse(body) as SlackPayload;
+	if (parsedBody.command) {
+		return handleSlashCommand(parsedBody as SlackSlashCommandPayload);
+	}
+	
+	return new Response('{"message": "helo slack"}', { status: 200, headers: { 'Content-Type': 'application/json' } })
 };
