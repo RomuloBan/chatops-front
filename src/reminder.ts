aa03d52d40236c647f2bf66f890b9b7f01dc56ee
romulo.banares <romulo.banares.contractor@bbva.com>
2024-10-25 20:31:44 +0200

    add reminder logic

---
+++ src/reminder.ts
@@ -0,0 +1,33 @@
+import type { Config } from "@netlify/functions"
+import { getNewItems } from "./util/notion";
+import { blocks, slackApi } from "./util/slack";
+
+const postnewNotionItemsToSlack = async () => {
+  const items = await getNewItems();
+
+  await slackApi("chat.postMessage", {
+    channel: '',
+    blocks: [
+      blocks.section({
+        text: [
+          'Here are the opinions awaiting judgment:',
+          '',
+          ...items.map((item) => `- ${item.opinion} (spice level: ${item.spiceLevel})`),
+          '',
+          `See all items: <https://notion.com/${process.env.NOTION_DATABASE_ID}| in Notion>`,
+        ].join('\n'),
+      }),
+    ],
+  });
+
+  return new Response('', { status: 200 });
+}
+
+export default async (req: Request) => {
+  const { next_run } = await req.json()
+  console.log("Received event! Next invocation at:", next_run)
+}
+
+export const config: Config = {
+  schedule: "@hourly"
+}
